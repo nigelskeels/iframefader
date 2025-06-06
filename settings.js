e3b125d4afa404e7064cfeb1d82ca991f6fb7f4a
nigelskeels <skeels@hotmail.com>
2025-06-06 11:29:01 +0100

    Update

---
+++ settings.js
@@ -0,0 +1,10 @@
+var settings = {
+  pages: [
+    { page: "http://www.hypermodernism.com/" },
+    { page: "http://www.hypermodernism.com/cv" },
+    { page: "http://www.hypermags.com" },
+    { page: "http://www.bbc.com" },
+  ],
+  animationSpeed: 4,
+  ipInfo: "My IP: 10.55.134.???",
+}
